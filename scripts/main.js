2541bfc51a93fad184edf17fffccdb543e972e58
Ravi Kumar Nimmi <rvkmr102@gmail.com>
2015-12-04 16:30:15 +0530

    calculator

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,41 @@
-var myImage = document.querySelector('img');
+//var myImage = document.querySelector('img');
 
-myImage.onclick = function() {
-    var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/a.png') {
-      myImage.setAttribute ('src','images/cool.jpg');
-    } else {
-      myImage.setAttribute ('src','images/a.png');
-    }
+//myImage.onclick = function() {
+    //var mySrc = myImage.getAttribute('src');
+    //if(mySrc === 'images/a.png') {
+      //myImage.setAttribute ('src','images/cool.jpg');
+    //} else {
+      //myImage.setAttribute ('src','images/a.png');
+    //}
+//}
+
+//var myButton = document.querySelector('button');
+//var myHeading = document.querySelector('h1');
+
+//function setUserName() {
+  //var myName = prompt('Please enter your name.');
+  //localStorage.setItem('name', myName);
+  //myHeading.textContent = 'Mozilla is cool, ' + myName;
+//}
+
+//if(!localStorage.getItem('name')) {
+  //setUserName();
+//} else {
+  //var storedName = localStorage.getItem('name');
+  //myHeading.textContent = 'Mozilla is cool, ' + storedName;
+//}
+var multiply = function()
+{
+  var num1 = document.getElementById('num1');
+  var num2 = document.getElementById('num2');
+  var sum = document.getElementById('product');
+  var s = parseInt(num1.value) * parseInt(num2.value);
+  sum.value = s;
 }
 
-var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h1');
-
-function setUserName() {
-  var myName = prompt('Please enter your name.');
-  localStorage.setItem('name', myName);
-  myHeading.textContent = 'Mozilla is cool, ' + myName;
-}
-
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  var storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Mozilla is cool, ' + storedName;
-}
-
+var myButton = document.getElementById("multiply_button");
 myButton.onclick = function() {
-  setUserName();
+	multiply();
 }
 
 
